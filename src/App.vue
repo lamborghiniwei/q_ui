27df19f32e005b2093e0f2363db78ddd6ccf5455
ls <478769831@qq.com>
2020-08-13 22:27:38 +0800

    dander button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <qui-button type="primary">主要按钮</qui-button>
     <qui-button type="success">成功按钮</qui-button>
+    <qui-button type="danger">危险按钮</qui-button>
   </div>
 </template>
 
