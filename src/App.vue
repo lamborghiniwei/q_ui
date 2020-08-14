1876bf55ca6a1d09f6074bddddd3930d52ffe455
ls <478769831@qq.com>
2020-08-14 20:55:47 +0800

    info button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <qui-button type="primary">主要按钮</qui-button>
     <qui-button type="success">成功按钮</qui-button>
     <qui-button type="danger">危险按钮</qui-button>
+    <qui-button type="info">信息按钮</qui-button>
   </div>
 </template>
 
