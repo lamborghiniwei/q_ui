b475441471269a8f1bc5e39aff5bfa45ce12bc92
ls <478769831@qq.com>
2020-08-19 23:57:48 +0800

    普通按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <qui-button type="primary">主要按钮</qui-button>
+    <qui-button type="primary">普通按钮</qui-button>
     <qui-button type="success">成功按钮</qui-button>
     <qui-button type="danger">危险按钮</qui-button>
     <qui-button type="info">信息按钮</qui-button>
